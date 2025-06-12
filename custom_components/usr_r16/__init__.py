15d257d335269fa38be0a85dfa9ce46055d0b97b
Elwinmage <52315693+Elwinmage@users.noreply.github.com>
2025-06-12 09:51:41 +0200

    Update __init__.py COMPATIBLE TO 2025.6

--- custom_components/usr_r16/__init__.py
+++ custom_components/usr_r16/__init__.py
@@ -129,9 +129,9 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         hass.data[DOMAIN][entry.entry_id][DATA_DEVICE_REGISTER] = client
 
         # Load entites
-        hass.async_create_task(
-            await hass.config_entries.async_forward_entry_setups(entry, ["switch"])
-        )
+        #hass.async_create_task(
+        await hass.config_entries.async_forward_entry_setups(entry, ["switch"])
+        #)
 
         _LOGGER.info("Connected to USR-R16 device: %s", address)
 
